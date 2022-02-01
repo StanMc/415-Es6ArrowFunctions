99bdb5ee3819d7cd2eac3b4954f01c1e4b54cb65
McBarbar <42481948+StanMc@users.noreply.github.com>
2022-02-01 06:42:02 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+var numbers = [3, 56, 2, 48, 5];
+
+////Map -Create a new array by doing something with each item in an array.
+const newNumbersSquare = numbers.map((x) => x * 2);
+console.log(newNumbersSquare);
+
+//////Filter - Create a new array by keeping the items that return true.
+const newNumbersFilter = numbers.filter((num) => num < 10);
+console.log(newNumbersFilter);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+var newNumberReduce = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+console.log(newNumberReduce);
+
+////Find - find the first item that matches from an array.
+const newNumberFind = numbers.find((num) => num > 10);
+console.log(newNumberFind);
+
+////FindIndex - find the index of the first item that matches.
+const newNumberIndex = numbers.findIndex((num) => num > 10);
+console.log(newNumberIndex);
